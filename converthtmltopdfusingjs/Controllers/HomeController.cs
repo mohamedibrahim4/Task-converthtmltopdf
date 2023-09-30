59be440ee8564660d5926dece6a67b9ce47a69f8
mohamedibrahim4 <mohamedatlm297@gmail.com>
2023-09-30 23:56:59 +0400

    first commit

---
+++ converthtmltopdfusingjs/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using converthtmltopdfusingjs.Models;
+
+namespace converthtmltopdfusingjs.Controllers
+{
+    public class HomeController : Controller
+    {
+        northwindEntities DB = new northwindEntities();
+        public ActionResult Index()
+        {
+
+            return View(DB.Customers.Take(10).ToList());
+        }
+
+        public ActionResult order(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var order = DB.Orders.Where(a => a.CustomerID == id).ToList();
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+    }
+}
---
+++ converthtmltopdfusingjs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="converthtmltopdfusingjs.MvcApplication" Language="C#" %>
